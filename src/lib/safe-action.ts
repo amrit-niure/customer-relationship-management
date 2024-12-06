e44bb58f1446ecbd39589e764d24b3878fd9d63b
Amrit Niure <niureamrit2@gmail.com>
2024-12-06 18:05:26 +1100

    -m

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,4 +1,4 @@
-import { env } from "@/env";
+// import { env } from "@/env";
 import { rateLimitByKey } from "@/lib/limiter";
 import { assertAuthenticated } from "@/lib/session";
 import { PublicError } from "@/use-cases/errors";
@@ -7,7 +7,7 @@ import { createServerActionProcedure } from "zsa";
 function shapeErrors({ err }: any) {
     const isAllowedError = err instanceof PublicError;
     // let's all errors pass through to the UI so debugging locally is easier
-    const isDev = env.NODE_ENV === "development";
+    const isDev = process.env.NODE_ENV === "development";
     if (isAllowedError || isDev) {
         console.error(err);
         return {
