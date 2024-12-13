c6a9aa683b3468649d5cc2001392fab47a482a2d
amrit-niure <niureamrit2@gmail.com>
2024-12-13 11:54:51 +1100

    constant changed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "drizzle-kit"
 
 
 if(!process.env.DATABASE_URL){
-  throw new Error('Database URL not found. Please set DRIZZLE_DATABASE_URL in your environment variables.');
+  throw new Error('Database URL not found. Please set DATABASE_URL in your environment variables.');
 }
 
 export default defineConfig({
