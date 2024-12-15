eddf0522fcd8ffab886a1cd59eead1d52ef8b993
Amrit Niure <niureamrit2@gmail.com>
2024-12-15 17:15:28 +1100

    cron job secured with headers

--- src/app/api/cron-jobs/visa-expiry/route.ts
+++ src/app/api/cron-jobs/visa-expiry/route.ts
@@ -1,10 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { eq, lt, and, inArray } from 'drizzle-orm';
 import { database } from '@/db/connection';
 import { appointments } from '@/db/schema';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+
+  const token = request.headers.get('cron-job-token');
+
+  if (!token) {
+    return NextResponse.json({ error: 'No Token provided' }, { status: 401 });
+  }
+
+  if(token !== process.env.CRON_JOB_SECRET) {
+    return NextResponse.json({ error: 'Invalid token provided' }, { status: 401 });
+  }
   try {
+    
     // Convert current date to a Date object
     const currentDate = new Date().toISOString()
     // Find appointments that are expired
